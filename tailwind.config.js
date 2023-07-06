6c77f1580ad46ea3e344d7b0abb55066a9d62e75
en3sis <940170+en3sis@users.noreply.github.com>
2023-07-06 18:12:57 +0200

    ops: upgrades prettier to v3, removes the node-inspect from yarn dev command

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  content: ['./public/**/*.html', './pages/**/*.{js,jsx,ts,tsx}', './app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './public/**/*.html',
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './app/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
   darkMode: 'class', // or 'media' or 'class'
 }
