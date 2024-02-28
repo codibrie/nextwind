6df34759a32ee01cd2c47096d5525291ad87d3a7
Israel <israelrojas626@gmail.com>
2024-02-28 12:30:46 -0400

    format

--- components/user.tsx
+++ components/user.tsx
@@ -7,14 +7,11 @@ const Login = (props) => {
   const { user: data } = props
   const user = data?.user
 
-
   return (
     <div className='flex items-center gap-2'>
       {Boolean(!user) && (
         <div className='flex gap-3'>
-          <button onClick={() => DiscordLogin()}>
-            Sign In with Discord
-          </button>
+          <button onClick={() => DiscordLogin()}>Sign In with Discord</button>
         </div>
       )}
 
@@ -22,7 +19,14 @@ const Login = (props) => {
         <>
           <p className='text-sm font-medium'>Welcome, {user?.user_metadata?.name}</p>
           {user?.user_metadata?.avatar_url ? (
-            <Image width={32} height={32} onClick={() => handleLogout()} className='rounded-full border-2 cursor-pointer' src={user?.user_metadata?.avatar_url} alt='avatar' />
+            <Image
+              width={32}
+              height={32}
+              onClick={() => handleLogout()}
+              className='rounded-full border-2 cursor-pointer'
+              src={user?.user_metadata?.avatar_url}
+              alt='avatar'
+            />
           ) : (
             <div className='w-8 h-8 rounded-full border-2' />
           )}
